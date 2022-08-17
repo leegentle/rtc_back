ed152b50564f5ae2e653e53de77d9356e4c51f7a
gentlee <joong6666@naver.com>
2022-08-17 15:24:59 +0900

    offer done

--- src/app.ts
+++ src/app.ts
@@ -15,9 +15,29 @@ const io = socketIO(server);
 
 // socketio 문법
 io.on("connection", (socket: any) => {
-  console.log("User connected");
-  socket.on("disconnect", () => {
-    console.log("User disconnect");
+  socket.on("join_room", (roomName: string) => {
+    console.log("방이름 : " + roomName);
+    socket.join(roomName);
+    socket.to(roomName).emit("welcome");
+  });
+
+  socket.on("offer", (offer: any, roomName: string) => {
+    console.log("오퍼");
+    socket.to(roomName).emit("offer", offer);
+  });
+  socket.on("answer", (answer: any, roomName: string) => {
+    socket.to(roomName).emit("answer", answer);
+  });
+  socket.on("ice", (ice: any, roomName: string) => {
+    socket.to(roomName).emit("ice", ice);
+  });
+
+  socket.on("msg", (msgObj: any, roomName: string) => {
+    socket.to(roomName).emit("msg", msgObj);
+  });
+
+  socket.on("disconnect", function () {
+    // console.log("user disconnected: ", socket.id);
   });
 });
 
