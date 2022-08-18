fced8f9881b5b5e843204f2e6e82ceb57642b99a
indjback <joong6666@naver.com>
2022-08-18 22:11:06 +0900

    다대다 통신중

--- src/app.ts
+++ src/app.ts
@@ -2,29 +2,45 @@ const express = require("express");
 const http = require("http");
 const socketIO = require("socket.io");
 
+interface offerData {
+  sendID: string;
+  receiceID: string;
+  offer: RTCPeerConnection;
+}
+
 // localhost 포트 설정
 const port = 4002;
-
 const app = express();
-
-// server instance
 const server = http.createServer(app);
-
-// socketio 생성후 서버 인스턴스 사용
 const io = socketIO(server);
+
+let users: { [roomName: string]: { socketId: string; nick: string }[] } = {};
 
 // socketio 문법
 io.on("connection", (socket: any) => {
-  socket.on("join_room", (roomName: string) => {
-    console.log("방이름 : " + roomName);
+  socket.on("join_room", (data: { roomName: string; nick: string }) => {
+    const { roomName, nick } = data;
+    const user = { socketId: socket.id, nick: nick };
+
     socket.join(roomName);
-    socket.to(roomName).emit("welcome");
+
+    if (users[roomName]) {
+      users[roomName].push(user);
+      const others = users[roomName].filter(
+        (user) => user.socketId !== socket.id
+      );
+      io.to(socket.id).emit("otherUsers", others);
+    } else {
+      users[roomName] = [user];
+    }
   });
 
-  socket.on("offer", (offer: any, roomName: string) => {
+  socket.on("offer", (data: offerData) => {
+    const { sendID, offer, receiceID } = data;
     console.log("오퍼");
-    socket.to(roomName).emit("offer", offer);
+    io.to(receiceID).emit("offer", { offer, sendID });
   });
+
   socket.on("answer", (answer: any, roomName: string) => {
     socket.to(roomName).emit("answer", answer);
   });
@@ -33,6 +49,8 @@ io.on("connection", (socket: any) => {
   });
 
   socket.on("msg", (msgObj: any, roomName: string) => {
+    console.log(msgObj);
+    console.log(roomName);
     socket.to(roomName).emit("msg", msgObj);
   });
 
